ab737f1ef65ea902f406dd661b61dc95b30d1989
Oscar Argueta <aradevs.argueta@gmail.com>
2023-12-01 20:14:24 -0600

    update: configured initial app theme

--- app/src/main/java/com/aradevs/pokeapp/ui/theme/Color.kt
+++ app/src/main/java/com/aradevs/pokeapp/ui/theme/Color.kt
@@ -2,10 +2,25 @@ package com.aradevs.pokeapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+//region Custom Light Colors
+val deepBlue = Color(0xFF005FFF)
+val mainText = Color(0xFF01426A)
+val secondaryText = Color(0xFF7C7C7C)
+val mainSurface = Color(0xFFF8F8F8)
+val cardSurface = Color(0xFFFFFFFF)
+val yellowAccent = Color(0xFFFFC600)
+val blueAccent = Color(0xFF2C85BC)
+val darkGray = Color(0xFF404040)
+val black = Color(0xFF141414)
+//end region
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+//region Custom Dark Colors
+val deepBlueDarkTheme = Color(0xFF1E88E5)
+val mainTextDarkTheme = Color(0xFFE3F2FD)
+val secondaryTextDarkTheme = Color(0xFFBDBDBD)
+val mainSurfaceDarkTheme = Color(0xFF121212)
+val cardSurfaceDarkTheme = Color(0xFF1E1E1E)
+val yellowAccentDarkTheme = Color(0xFFFFEB3B)
+val blueAccentDarkTheme = Color(0xFF03A9F4)
+val darkGrayDarkTheme = Color(0xFF757575)
+val blackDarkTheme = Color(0xFFE0E0E0)
